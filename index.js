05ee8e83cc2be57a334d37b8a01dd87616637685
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-18 23:24:24 +0530

    index.js: setup middlewares and routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
 const connection = require('./Database/Config/connection')
-
 
 //connect to database
 connection()
@@ -13,10 +13,18 @@ db.once('open', function() {
 });
 
 
+// middlewares
+app.use(bodyparser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+const user = require('./Router/User/user')
+const admin = require('./Router/Admin/admin')
 
 
 
-
+app.use("/user",user)
+app.use("/admin",admin)
 
 
 
