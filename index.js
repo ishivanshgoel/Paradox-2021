47f820a9af62a4f0caaca439354e17a82e8e866e
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-18 20:26:40 +0530

    index.js: setup database connection

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const connection = require('./Database/Config/connection')
+
+
+//connect to database
+connection()
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to database!!')
+});
+
+
+
+
 
 
 
