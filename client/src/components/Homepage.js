a685e57db0241228a870bdf6ed6d3c913f183c69
TejasV58 <64249206+TejasV58@users.noreply.github.com>
2021-02-22 03:18:56 +0530

    homepage paradox added

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './homepage.css'
 
 function Homepage() {
     return (
         <div>
-            <h1>Homepage</h1>
+            <svg viewBox="0 0 3500 500" className="paradox ">
+                <text x="50%" y="50%" fill="transparent" text-anchor="middle">PARADOX</text>
+            </svg>
+            <div className="button-outer-box d-flex justify-content-center">
+                <button className="btn  mx-5 my-3 px-5 py-2 homebtn">SIGNUP</button>
+                <button className="btn  mx-5 my-3 px-5 py-2 homebtn">REGISTER</button>
+                <button className="btn  mx-5 my-3 px-5 py-2 homebtn">DISCORD</button>
+            </div>
+            <div className="about-section">
+                <h1>ABOUT</h1>
+            </div>
         </div>
     )
 }
