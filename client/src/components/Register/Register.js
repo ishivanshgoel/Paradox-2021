4fd11ddf61970e0d4dfc3a8b1dbdcb0245aa696c
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-22 14:56:56 +0530

    Register.js: Rectify errors and made form function

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,36 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./register_css/register.css";
 /**
  * Level 1 Route - /register
- * 
+ * @author TanayBhadula, ishivanshgoel
  */
-/**
-@author: TanayBhadula
-*/
 
 function Register() {
-    return (
-        <div>
-          <div class="container p-0">
-            <div class="row">
-              <div class="col-12 offset-lg-3 col-lg-6 offset-md-2 col-md-8 p-0">
-                <form class="register__box">
-                  <h1>Register</h1>
-                  <input type="text" name="name" placeholder="Name" required="">
-                  <input type="text" name="userName" placeholder="Username" required="">
-                  <input type="text" name="email" placeholder="Email" required="">
-                  <input type="text" name="discord" placeholder="Discord Username" required="">
-                  <input type="text" name="institutionName" placeholder="Institution Name" required="">
-                  <input type="password" name="password" placeholder="Password" required="">
-                  <input type="password" name="cpassword" placeholder="Confirm Password" required="">
-                  <input type="submit" name="login" value="Login">
-                </form>
-              </div>
-            </div>
-          </div>
+
+  // state variables
+  const [name, setName] = useState(null)
+  const [username, setUsername] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [discord, setDiscord] = useState(null)
+  const [institutionName, setinstitutionName] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [cpassword, setcPassword] = useState(null)
+
+  // handle form submission
+  let handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(name, username, email, discord, institutionName, password, cpassword)
+  }
+
+  return (
+    <div class="container p-0" onSubmit={handleSubmit}>
+      <div class="row">
+        <div class="col-12 offset-lg-3 col-lg-6 offset-md-2 col-md-8 p-0">
+          <form class="register__box">
+            <h1>Register</h1>
+            <input type="text" name="name" placeholder="Name" required onChange={(event) => setName(event.target.value)} />
+            <input type="text" name="userName" placeholder="Username" required onChange={(event) => setUsername(event.target.value)} />
+            <input type="text" name="email" placeholder="Email" required onChange={(event) => setEmail(event.target.value)} />
+            <input type="text" name="discord" placeholder="Discord Username" required onChange={(event) => setDiscord(event.target.value)} />
+            <input type="text" name="institutionName" placeholder="Institution Name" required onChange={(event) => setinstitutionName(event.target.value)} />
+            <input type="password" name="password" placeholder="Password" required onChange={(event) => setPassword(event.target.value)} />
+            <input type="password" name="cpassword" placeholder="Confirm Password" required onChange={(event) => setcPassword(event.target.value)} />
+            <input type="submit" name="login" />
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Register
