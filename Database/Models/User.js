60f707c3acbd04a10a8580d96de8b56d966d2f6b
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-23 18:01:17 +0530

    User.js: method to verify password

--- Database/Models/User.js
+++ Database/Models/User.js
@@ -71,6 +71,16 @@ UserSchema.pre('save',async function(next){
 
 })
 
+UserSchema.methods.isValidPassword = async function (password) {
+
+    try{
+        return await bcrypt.compare(password, this.password)
+    } catch(error){
+        throw(error)
+    }
+    
+}
+
 const User = mongoose.model('user', UserSchema)
 
 module.exports = User
