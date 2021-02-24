8d3a765d02ba03872de28c8938ac7ac07a8d0866
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-24 12:43:21 +0530

    Homepage.js: Create a different division for font and buttons on top and set it's height to 100vh.

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -18,19 +18,21 @@ function Homepage() {
 
     return (
         <div className="row">
-            <div class="col-12">
-                <svg viewBox="0 0 3500 500" className="paradox ">
-                    <text x="50%" y="50%" fill="transparent" text-anchor="middle">PARADOX</text>
-                </svg>
-                <div className="button-outer-box d-flex justify-content-center">
-                    <div class="col-md-2 col-lg-2 ">
-                        <button class=" homebtn" onClick={handleRedirect}>LOGIN</button>
-                    </div>
-                     <div class="col-md-2 col-lg-2">
-                        <button class=" homebtn d-flex  justify-content-center" id="discord"><img src={discordlogo} id="DiscordLogo" alt=""/>Discord</button>
-                    </div>
-                     <div class="col-md-2 col-lg-2">
-                        <button class=" homebtn" onClick={handleRedirect}>Register</button>
+            <div className="col-12">
+                <div className="homepage__main">
+                    <svg viewBox="0 0 3500 500" className="paradox ">
+                        <text x="50%" y="50%" fill="transparent" text-anchor="middle">PARADOX</text>
+                    </svg>
+                    <div className="button-outer-box d-flex justify-content-center">
+                        <div className="col-md-2 col-lg-2 ">
+                            <button className=" homebtn" name="user" onClick={handleRedirect}>LOGIN</button>
+                        </div>
+                        <div className="col-md-2 col-lg-2">
+                            <button className=" homebtn d-flex  justify-content-center" id="discord"><img src={discordlogo} id="DiscordLogo" alt=""/>Discord</button>
+                        </div>
+                        <div className="col-md-2 col-lg-2">
+                            <button className=" homebtn" name="register" onClick={handleRedirect}>Register</button>
+                        </div>
                     </div>
                 </div>
             </div>
