92264dd5aba2f7aee4a2724a45934c752909b48a
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-27 14:23:56 +0530

    Requests: axios functions for Post and Get Requests

---
+++ client/src/Helper/GetRequest.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:5000"
+
+const endPoints = {
+    // user auth
+    leaderboard: "/user/leaderboard",
+    logout: "/user/logout",
+    nextlevel: "/user/nextlevel"
+}
+
+var urlBuilder = (endPoint, id = null) => {
+
+    if (id != null) {
+        return BASE_URL + endPoints[endPoint] + "/" + id
+    }
+    else return BASE_URL + endPoints[endPoint]
+
+}
+
+async function GET_Request(endPoint, id = null) {
+
+    let token = localStorage.getItem("token")
+
+    return axios.get(urlBuilder(endPoint, id),{
+            headers: { 'Authorization': token ? `Bearer ${token}` : '', 'Content-Type': 'application/json' }
+        }).then((response) => {
+            console.log("Response in get request ", response)
+            return response
+        }).catch((err) => {
+            return err.message
+        })
+
+}
+
+export default GET_Request
