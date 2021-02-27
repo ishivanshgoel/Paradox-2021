053ca2cb65b9802e873fc279a6f45ff08c38511c
Tanay Bhadula <53116650+TanayBhadula@users.noreply.github.com>
2021-02-27 11:59:00 +0530

    Update Leaderboard.js

--- client/src/components/User/Leaderboard.js
+++ client/src/components/User/Leaderboard.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./leaderboard_css_css/leaderboard.css";
+
 /**
  * Level 2 Route - /user/leaderboard
- * @author TanayBhadula
+ *
  */
 
 function Leaderboard() {
