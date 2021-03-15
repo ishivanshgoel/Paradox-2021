30bcaf22a69ac69e6a7518fe121e77fdd3f8346e
Shivansh Goel <shivanshgoel21@gmail.com>
2021-03-15 17:42:10 +0530

    ImageView.js: Form to modify question.js

--- client/src/components/Dashboard/ImageView.js
+++ client/src/components/Dashboard/ImageView.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './CSS/ImageView.css'
 
-function ImageView({id, link}) {
+function ImageView({ id, link }) {
+
+    const handleSubmit = ()=>{
+        alert('Handle Submit')
+    }
+
     return (
         <div>
             <div className="row">
@@ -10,7 +15,18 @@ function ImageView({id, link}) {
                         <img src={link} alt={link} />
                     </div>
                     <div className="col-12 admin__imageview__buttons">
-                        <button id={id}>Delete</button>
+                        <form onSubmit={handleSubmit} style={{ margin: "40px" }}>
+                            <div className="mb-3">
+                                <label className="form-label">Question</label>
+                                <input type="text" className="form-control" aria-describedby="emailHelp" style={{ backgroundColor: "rgb(7,23,44)" }} />
+                                <div className="form-text">Modify the above question</div>
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">New Answer</label>
+                                <input type="text" className="form-control" style={{ backgroundColor: "rgb(7,23,44)" }} />
+                            </div>
+                            <button type="submit" className="btn btn-primary" style={{ backgroundColor: "rgb(7,23,44)" }}>Modify</button>
+                        </form>
                     </div>
                 </div>
             </div>
