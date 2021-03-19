10e8e2b491f0e7da60ecc606197f0953c6dadfbe
Shivansh Goel <shivanshgoel21@gmail.com>
2021-03-19 19:59:44 +0530

    ** Deploy 19-03-2020 **

---
+++ client/build/precache-manifest.585dd93876fd91c1d0a2e369494b107e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f35aac776466c00ed41",
+    "url": "/static/css/main.c92b1306.chunk.css"
+  },
+  {
+    "revision": "1f35aac776466c00ed41",
+    "url": "/static/js/main.eb14c4be.chunk.js"
+  },
+  {
+    "revision": "f31463017cb89a7bb34e",
+    "url": "/static/js/runtime~main.6672a354.js"
+  },
+  {
+    "revision": "8c2451243a391af77ad6",
+    "url": "/static/css/2.6b4c2d40.chunk.css"
+  },
+  {
+    "revision": "8c2451243a391af77ad6",
+    "url": "/static/js/2.48f033f5.chunk.js"
+  },
+  {
+    "revision": "82aa28d81fff05e073f7",
+    "url": "/static/js/3.8c9c5e95.chunk.js"
+  },
+  {
+    "revision": "f63d4739339845001d0fd4d7132cca72",
+    "url": "/static/media/homepage_logo.f63d4739.svg"
+  },
+  {
+    "revision": "4101a09593eca4330a3d022234b85f5a",
+    "url": "/static/media/logo6.4101a095.png"
+  },
+  {
+    "revision": "564741cda237264a2fe3ea35d2cda78a",
+    "url": "/static/media/IEEE_CS1.564741cd.png"
+  },
+  {
+    "revision": "4ce4d4ecdb30d7a184f0211b9f59fec1",
+    "url": "/static/media/instagram.4ce4d4ec.png"
+  },
+  {
+    "revision": "b8e090c3d4ebf071ebac5f81dafce61c",
+    "url": "/static/media/github.b8e090c3.png"
+  },
+  {
+    "revision": "6c34acef638c34d9900b17b8c9c0e24c",
+    "url": "/static/media/ieeecsvit.6c34acef.png"
+  },
+  {
+    "revision": "64cae93602dd6e569155b4a0a99eab7a",
+    "url": "/static/media/github-logo.64cae936.png"
+  },
+  {
+    "revision": "483033dd43a06dac76f3835461b0e85c",
+    "url": "/static/media/email-logo.483033dd.png"
+  },
+  {
+    "revision": "54e828533ab1352867472b89f329d97d",
+    "url": "/static/media/shivanshgoel.54e82853.jpeg"
+  },
+  {
+    "revision": "eba952dc2471ac6b64aea5756900a985",
+    "url": "/static/media/sanikakulkarni.eba952dc.jpg"
+  },
+  {
+    "revision": "853256917e10014cc8d7bc81a4055d3a",
+    "url": "/static/media/tanaybhadula.85325691.jpeg"
+  },
+  {
+    "revision": "98b3ab4d17f0bc4c7f50fdd73c31566d",
+    "url": "/static/media/tejasimage.98b3ab4d.jpeg"
+  },
+  {
+    "revision": "6ae32332d26216358625508a16d91a66",
+    "url": "/static/media/demo.6ae32332.jpg"
+  },
+  {
+    "revision": "4718be3958c17aab2844369d1f60ce18",
+    "url": "/static/media/logo1_wobg1.4718be39.png"
+  },
+  {
+    "revision": "05f8617d5abf39bed2b3babfa13fe0de",
+    "url": "/static/media/discord.05f8617d.svg"
+  },
+  {
+    "revision": "97f6ed238effa6300c9119f1e18a721f",
+    "url": "/static/media/logoTagline.97f6ed23.svg"
+  },
+  {
+    "revision": "61874370fe1e1d3475d4d120a31b595e",
+    "url": "/index.html"
+  }
+];
