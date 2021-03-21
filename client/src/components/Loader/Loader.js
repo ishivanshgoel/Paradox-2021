92b46332bba2ea4a9cd67b55695c850004c4e067
Shivansh Goel <shivanshgoel21@gmail.com>
2021-03-21 21:16:19 +0530

    Loader.js: change loader

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -6,17 +6,7 @@ import loaderlogo from "../logos/loaderimage.png";
 function Loader() {
     return (
         <div className="loader">
-            <div id="particles-background" className="vertical-centered-box"></div>
-            <div id="particles-foreground" className="vertical-centered-box"></div>
-
-            <div className="vertical-centered-box">
-                <div className="content">
-                <div className="loader-circle"></div>
-                <div className="loader-line-mask">
-                    <div className="loader-line"></div>
-                </div>
-                <img className="loader-image" src={loaderlogo} width="60" height="60" />
-                </div>
+            <div className="spinner-border text-light" style={{ width: "5rem", height: "5rem" }} role="status">
             </div>
         </div>
     )
