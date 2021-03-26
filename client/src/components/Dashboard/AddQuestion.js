88a5fd9d5ed685eae899527ee031d3f10e8956d5
Shivansh Goel <shivanshgoel21@gmail.com>
2021-03-26 17:43:11 +0530

    AddQuestion.js: add new question from admin panel

--- client/src/components/Dashboard/AddQuestion.js
+++ client/src/components/Dashboard/AddQuestion.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+// Requests
+import POST_Request from '../../Helper/PostRequest'
+
+
+/**
+ * @author ishivanshgoel
+ */
 
 function AddQuestion() {
 
-    let handleSubmit = (event) =>{
+    const[image, setImage] = useState(null)
+    const[answer, setAnswer] = useState(null)
+    const[level, setLevel] = useState(null)
+
+    let handleSubmit = async (event) =>{
         event.preventDefault()
-        alert('Request to add new question')
+        const response = await POST_Request('add', {
+            imageUrl: image,
+            answer: answer,
+            levelNumber: level
+        })
+        if(response.data && response.data.message==="Saved"){
+            alert('Success: Request Fulfilled')
+        } else{
+            alert('Error ' + response)
+        } 
     }
 
     return (
         <div className="admin__addquestion">
-            <form onSubmit={handleSubmit} style={{margin:"40px"}}>
+            <form onSubmit={handleSubmit} style={{margin:"60px"}}>
                 <div className="mb-3">
                     <label className="form-label">Image</label>
-                    <input type="text" className="form-control" aria-describedby="emailHelp" style={{backgroundColor:"rgb(7,23,44)"}} />
+                    <input type="text" className="form-control" aria-describedby="emailHelp" style={{backgroundColor:"rgb(7,23,44)"}} onChange={(event)=>setImage(event.target.value)}/>
                     <div className="form-text">Link of the Image.</div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Answer</label>
-                    <input type="password" className="form-control" style={{backgroundColor:"rgb(7,23,44)"}} />
+                    <input type="text" className="form-control" style={{backgroundColor:"rgb(7,23,44)"}} onChange={(event)=>setAnswer(event.target.value)}/>
                 </div>
-                <button type="submit" className="btn btn-primary" style={{backgroundColor:"rgb(7,23,44)"}}>Add</button>
+                <div className="mb-3">
+                    <label className="form-label">Level</label>
+                    <input type="text" className="form-control" style={{backgroundColor:"rgb(7,23,44)"}} onChange={(event)=>setLevel(event.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary" style={{backgroundColor:"rgb(7,23,44)"}} >Add</button>
             </form>
         </div>
     )
